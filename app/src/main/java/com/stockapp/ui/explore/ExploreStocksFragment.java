9cd0f2e566ffc9913cb7b174194147b99be7fde5
saitejdandge <saitejdandge1@gmail.com>
2021-01-23 18:29:02 +0530

    Explore Stock Fragment

--- app/src/main/java/com/stockapp/ui/explore/ExploreStocksFragment.java
+++ app/src/main/java/com/stockapp/ui/explore/ExploreStocksFragment.java
@@ -7,17 +7,26 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.sdk.ViewModelProviderFactory;
 import com.sdk.base.BaseFragment;
 import com.stockapp.R;
+import com.stockapp.adapters.StockListAdapter;
 
 import javax.inject.Inject;
 
 public class ExploreStocksFragment extends BaseFragment<ExploreStocksViewModel> {
 
+    RecyclerView recyclerView;
+    private View loadingView;
+    private View errorView;
+    private SwipeRefreshLayout swipeRefreshLayout;
     @Inject
     ViewModelProviderFactory viewModelProviderFactory;
+
+    private StockListAdapter stockListAdapter;
 
     public static ExploreStocksFragment newInstance() {
         return new ExploreStocksFragment();
@@ -28,6 +37,53 @@ public class ExploreStocksFragment extends BaseFragment<ExploreStocksViewModel> 
         return inflater.inflate(R.layout.explore_stocks_fragment, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+        loadData();
+    }
+
+    private void initViews(View view) {
+        stockListAdapter = new StockListAdapter();
+        recyclerView = view.findViewById(R.id.recyclerView);
+        loadingView = view.findViewById(R.id.loadingView);
+        errorView = view.findViewById(R.id.errorView);
+        swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
+        View retry = view.findViewById(R.id.retry);
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            swipeRefreshLayout.setRefreshing(false);
+            ExploreStocksFragment.this.getViewModel().invalidateList();
+        });
+        retry.setOnClickListener(v -> this.getViewModel().invalidateList());
+    }
+
+
+    private void loadData() {
+        getViewModel().getNetworkState().observe(getViewLifecycleOwner(), networkState -> {
+                    switch (networkState) {
+                        case SUCCESS:
+                            loadingView.setVisibility(View.GONE);
+                            recyclerView.setVisibility(View.VISIBLE);
+                            errorView.setVisibility(View.GONE);
+                            break;
+                        case LOADING:
+                            loadingView.setVisibility(View.VISIBLE);
+                            errorView.setVisibility(View.GONE);
+                            recyclerView.setVisibility(View.GONE);
+                            break;
+                        case ERROR:
+                            loadingView.setVisibility(View.GONE);
+                            errorView.setVisibility(View.VISIBLE);
+                            recyclerView.setVisibility(View.GONE);
+                            break;
+                    }
+                }
+        );
+        this.getViewModel().getStockList().observe(getViewLifecycleOwner(), posts -> {
+            stockListAdapter.submitList(posts);
+        });
+    }
 
     @NonNull
     @Override
--- app/src/main/java/com/stockapp/ui/explore/ExploreStocksViewModel.java
+++ app/src/main/java/com/stockapp/ui/explore/ExploreStocksViewModel.java
@@ -1,14 +1,50 @@
 package com.stockapp.ui.explore;
 
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
+
 import com.sdk.base.BaseViewModel;
-import com.stockapp.repositories.StockRepo;
+import com.sdk.network.FindQuery;
+import com.sdk.network.ResourceStatus;
+import com.stockapp.models.StockListItem;
+import com.stockapp.paging.StockPagedDataSourceFactory;
+import com.stockapp.services.StockService;
 
 import javax.inject.Inject;
 
 public class ExploreStocksViewModel extends BaseViewModel {
-    @Inject
-    ExploreStocksViewModel(StockRepo stockRepo) {
 
+
+    private final StockPagedDataSourceFactory basePagedDataSourceFactory;
+    private final LiveData<ResourceStatus> networkState;
+    private final LiveData<PagedList<StockListItem>> stockList;
+
+
+    @Inject
+    ExploreStocksViewModel(StockService stockService) {
+        FindQuery findQuery = new FindQuery.Builder().build();
+        this.basePagedDataSourceFactory = new StockPagedDataSourceFactory(stockService, findQuery);
+        // Initial page size to fetch can also be configured here too
+        PagedList.Config config = new PagedList.Config.Builder().setPageSize(20).setEnablePlaceholders(true).setPrefetchDistance(5).build();
+        stockList = new LivePagedListBuilder(basePagedDataSourceFactory, config).build();
+        networkState = Transformations.switchMap(basePagedDataSourceFactory.getBasePagedDatasourceMutableLiveData(), dataSource -> dataSource.getNetworkState());
     }
+
+    void invalidateList() {
+        if (basePagedDataSourceFactory != null && basePagedDataSourceFactory.getDataSource() != null)
+            basePagedDataSourceFactory.getDataSource().invalidate();
+    }
+
+
+    LiveData<ResourceStatus> getNetworkState() {
+        return networkState;
+    }
+
+    LiveData<PagedList<StockListItem>> getStockList() {
+        return stockList;
+    }
+
 }
